82dbcbba021dd8c38cf0f2e04148c3d7a8a9db28
Tarikul Islam <ti.rasel@gmail.com>
2022-02-07 23:07:13 +0700

    Run time exception for filed class

--- src/test/java/ContactManagerTest.java
+++ src/test/java/ContactManagerTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ContactManagerTest {
@@ -10,4 +11,35 @@ public class ContactManagerTest {
         Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
         Assertions.assertEquals(1, contactManager.getAllContacts().size());
     }
+
+    @Test
+    @DisplayName("Should Fail with null firstname")
+    public void shouldNotCreateContactWhenFirstNameNull(){
+        ContactManager contactManager = new ContactManager();
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact(null, "Islam", "0987654680");
+        });
+    }
+
+    @Test
+    @DisplayName("Should Fail with null lastname")
+    public void shouldNotCreateContactWhenLastNameNull(){
+        ContactManager contactManager = new ContactManager();
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact("Tarikul", null, "0987654680");
+        });
+    }
+
+    @Test
+    @DisplayName("Should Fail with null phone no")
+    public void shouldNotCreateContactWhenPhoneNumberNull(){
+        ContactManager contactManager = new ContactManager();
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact("Tarikul", "Islam", null);
+        });
+    }
+
 }
